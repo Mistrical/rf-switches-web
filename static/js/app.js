0d2ad111378e86b7f1ebf95577e85294905fd3b1
Jaimesh Mistry <jaimeshm@gmail.com>
2015-09-15 22:35:13 +0100

    Now showing all switches from first pi

--- static/js/app.js
+++ static/js/app.js
@@ -3,9 +3,9 @@ var app = angular.module("app", []);
 app.controller("AppCtrl", function($http) {
     var app = this;
 
-    app.message = "Am I working?";
-
     $http.get("http://192.168.1.1:5000/api/switch").success (function (data) {
         app.switches = data.objects;
+
+
     })
 })
--- web-server.py
+++ web-server.py
@@ -6,7 +6,9 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file("index.html")
+    response = app.send_static_file("index.html")
+    print response
+    return response
 
 
 if __name__ == '__main__':
